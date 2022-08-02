e501ecba36afb589c5954e34fb5f63f06a78616b
sohanlal311 <sohansevta@gmail.com>
2022-08-02 08:58:33 +0530

    Added test drive code using protection proxy

--- src/com/sohan/g/proxypattern/remoteproxy/GumBallMachineTestDrive.java
+++ src/com/sohan/g/proxypattern/remoteproxy/GumBallMachineTestDrive.java
@@ -1,22 +1,50 @@
 package com.sohan.g.proxypattern.remoteproxy;
 
-import java.rmi.Naming;
-
 import com.sohan.f.statepattern.GumBallMachine;
+import com.sohan.f.statepattern.state.IState;
+
+import java.lang.reflect.Proxy;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
 
 public class GumBallMachineTestDrive {
 
-	public static void main(String[] args) {
-		try {
-			java.rmi.registry.LocateRegistry.createRegistry(1099);
-			System.out.println("RMI registry is ready...");
-			GumBallMachine gumBallMachine = new GumBallMachine(5);
-			Naming.rebind("rmi://localhost/gumBallRemote", gumBallMachine);
-			System.out.println("Server started...");
-			gumBallMachine.insertQuarter();
-			gumBallMachine.turnsCrank();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            GumBallMachine gumBallMachine = runUsingRemoteProxy();
+            runUsingProtectionProxy(gumBallMachine);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static GumBallMachine runUsingRemoteProxy() throws RemoteException, MalformedURLException {
+        java.rmi.registry.LocateRegistry.createRegistry(1099);
+        System.out.println("RMI registry is ready...");
+
+        GumBallMachine gumBallMachine = new GumBallMachine(5);
+        Naming.rebind("rmi://localhost/gumBallRemote", gumBallMachine);
+        System.out.println("Server started...");
+
+        gumBallMachine.insertQuarter();
+        gumBallMachine.turnsCrank();
+        return gumBallMachine;
+    }
+
+    private static void runUsingProtectionProxy(IGumBallMachineRemote gumBallMachine) throws RemoteException {
+        IGumBallMachineRemote gumBallMachineProxy = (IGumBallMachineRemote) Proxy.newProxyInstance(
+                gumBallMachine.getClass().getClassLoader(),
+                gumBallMachine.getClass().getInterfaces(),
+                (proxy, method, args1) -> {
+                    if (method.getName().startsWith("get")) {
+                        System.out.println("Called.........");
+                        return method.invoke(gumBallMachine, args1);
+                    }
+                    return null;
+                });
+        IState currentState = gumBallMachineProxy.getCurrentState();
+        System.out.println("Current state.... " + currentState);
+    }
+
 }
--- src/com/sohan/g/proxypattern/remoteproxy/GumBallMonitorTestDrive.java
+++ src/com/sohan/g/proxypattern/remoteproxy/GumBallMonitorTestDrive.java
@@ -1,16 +1,23 @@
 package com.sohan.g.proxypattern.remoteproxy;
 
-import java.rmi.Naming;
-
 import com.sohan.f.statepattern.GumBallMonitor;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 public class GumBallMonitorTestDrive {
 
-	public static void main(String[] args) throws Exception {
-		IGumBallMachineRemote gumBallMachine = (IGumBallMachineRemote) Naming
-				.lookup("rmi://localhost/gumBallRemote");
-		GumBallMonitor gumBallMonitor = new GumBallMonitor(gumBallMachine);
-		gumBallMonitor.getReport();
-	}
+    public static void main(String[] args) throws Exception {
+        runUsingRemoteProxy();
+    }
+
+    private static void runUsingRemoteProxy() throws NotBoundException, MalformedURLException, RemoteException {
+        IGumBallMachineRemote gumBallMachine = (IGumBallMachineRemote) Naming
+                .lookup("rmi://localhost/gumBallRemote");
+        GumBallMonitor gumBallMonitor = new GumBallMonitor(gumBallMachine);
+        gumBallMonitor.getReport();
+    }
 
 }
